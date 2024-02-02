08ec16ed1b071a9a8611ee395dc1b707e7f0a187
Felix Ouma <moxiemeyoh@gmail.com>
2024-02-02 21:29:26 +0300

    Add: clent Setup - react-js - tailwind css server folder

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-primary">
+      <h1 className="bg-red-500 text-yellow-400">Blog App with Tailwind</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./index.html", "./src/**/*.{js,jsx}"];
+export const mode = "jit";
+export const theme = {
+    extend: {
+        fontSize: {
+            'h1': '3rem',
+            'h2': '2.25rem',
+            'h3': '1.1875rem',
+            'h4': '1.5rem',
+            'p': '1rem',
+            'little': '0.8rem',
+        },
+        colors: {
+        primary: "#5e0000",
+        secondary: "#1e1c1f",
+        textNormal: "#ff746e",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        bgButton: "#ffbf3c",
+        bgButtonSecondary: "#ff6b66",
+        heroColor: "#F5F5DC",
+        heroTransparent: "rgba (50, 50, 50, 0.5)",
+        },
+        fontFamily: {
+        raleway: ["'Montserrat',sans-serif"],
+        },
+        backgroundImage: {
+            'hero-image': "url('./assets/image-7.jpg')",
+            'mobile-hero': "url('./assets/mobile-hero.jpg')",
+        }
+    },
+    screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+    },
+};
+export const plugins = [];
+
