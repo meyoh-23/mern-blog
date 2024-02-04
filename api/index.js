44bef8d2ff25f05060eb36112c7eda8fd50f2e31
Felix Ouma <moxiemeyoh@gmail.com>
2024-02-04 11:16:41 +0300

    Add: Connect to database - connection to MongoDB - seperate environement variables

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000...')
+// coonect to mongoDB database
+const conectDB = async (urlToDB) => {
+    try {
+        await mongoose.connect(urlToDB);
+        console.log("Db connection Successful")
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}...`);
+    conectDB(process.env.MONGO_DB)
 });
