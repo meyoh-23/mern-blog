ec7c8ca9bdc26346d477fe5ab1c3d9ee7950b08f
Felix Ouma <moxiemeyoh@gmail.com>
2024-02-26 15:40:47 +0300

    Add: Signup UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const Signup = () => {
     return (
         <div className="min-h-screen mt-20">
-            <div>
-                <div className="">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* left */}
+                <div className="flex-1">
                     <Link to="/"
                         className='self-center text-lg  font-semibold dark:text-white'
                     >
@@ -19,8 +21,47 @@ const Signup = () => {
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto officia unde aliquid, totam dignissimos molestias soluta pariatur,
                     </p>
                 </div>
-                <div>
-                right side
+                <div className="flex-1">
+                {/* right side */}
+                    <div className="">
+                        <form className="flex flex-col gap-4">
+                        <div>
+                            <Label value="Your username"/>
+                            <TextInput
+                            type="text"
+                            placeholder="Username"
+                            id="username"
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your email"/>
+                            <TextInput
+                            type="text"
+                            placeholder="name@company.com"
+                            id="email"
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your password"/>
+                            <TextInput
+                            type="text"
+                            placeholder="Password"
+                            id="password"
+                            />
+                        </div>
+                        <Button gradientDuoTone="purpleToPink" type="submit">
+                            Signup
+                        </Button>
+                        </form>
+                        <div className="flex gap-2 text-sm mt-5">
+                            <span>
+                                Have an account?
+                            </span>
+                            <Link to='/sign-in' className="text-blue-500">
+                                Sign In
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
