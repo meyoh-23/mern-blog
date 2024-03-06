124d412d35ca27641c9aa3466b735ea0aa3d960e
Felix Ouma <moxiemeyoh@gmail.com>
2024-03-06 05:51:52 +0300

    Add: Dashboard Components Initialization - DashSidebar - DashProfile - conditional rendering

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Auth from "./outh";
+import DashSidebar from "./DashSidebar";
+import DashProfile from "./DashProfile";
 
 
-export { Header, Footer, Auth };
+export { Header, Footer, Auth, DashProfile, DashSidebar };
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { DashProfile, DashSidebar } from '../components';
 
 const Dashboard = () => {
+    const location = useLocation();
+    const[tab, stTab] = useState('');
+
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search);
+        const  tabfromURL = urlParams.get('tab');
+        console.log(tabfromURL);
+    },[location.search]);
+
+
     return (
-        <div>Dashboard</div>
+        <div className=''>
+            <div className="">
+                {/* Sidebar */}
+                <DashSidebar/>
+            </div>
+            {/* profile... */}
+            {
+                tab == 'profile' && <DashProfile/>
+            }
+        </div>
     )
 }
 
