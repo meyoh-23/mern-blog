40dfdcd8620015229f1c11abc32f461f5db2f0c1
Felix Ouma <moxiemeyoh@gmail.com>
2024-03-09 19:29:57 +0300

    Add: Profile PAge UI - default values from redux state

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,9 +1,50 @@
+import { useSelector } from 'react-redux';
+import { TextInput, Button } from 'flowbite-react';
 
+export default function DashProfile () {
+  const { currentUser } = useSelector(state => state.user);
 
-const DashProfile = () => {
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form  className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+          <img  
+            src={currentUser.profilePicture} 
+            alt="user_profile" 
+            className='w-full h-full rounded-full border-8 border-[lightgray] object-cover'
+          />
+        </div>
+        <TextInput
+        type='text'
+        id='username'
+        placeholder='username'
+        defaultValue={currentUser.username}
+        />
+        <TextInput
+        typeemail
+        id='email'
+        placeholder='email'
+        defaultValue={currentUser.email}
+        />
+        <TextInput
+        type='password'
+        id='password'
+        placeholder='password'
+        />
+        <Button 
+        type='submit'
+        gradientDuoTone="purpleToBlue"
+        outline
+        >
+          Update
+        </Button>
+      </form>
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Signout</span>
+      </div>
+    </div>
   )
 }
 
-export default DashProfile;
