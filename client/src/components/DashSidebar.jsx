d42117c764b0b067b3345ee2de28f9d97cbf7373
Felix Ouma <moxiemeyoh@gmail.com>
2024-03-10 12:10:04 +0300

    Add: Image upload - display uploaded image to user - show upload progess, - display errors if any - upload image to firebase storage

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { HiUser, HiArrowSmRight } from 'react-icons/hi';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const DashSidebar = () => {
     const location = useLocation();
@@ -19,9 +19,17 @@ const DashSidebar = () => {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item active={ tab==="profile" } icon={HiUser} label="User" labelColor="dark">
+                    <Link to="/dashboard?tab=profile">
+                    <Sidebar.Item 
+                    active={ tab==="profile" } 
+                    icon={HiUser} label="User" 
+                    labelColor="dark"
+                    as='div'
+                    >
                         Profile
                     </Sidebar.Item>
+                    </Link>
+                    
                     <Sidebar.Item icon={HiArrowSmRight}  className="cursor-pointer ">
                         Sign out
                     </Sidebar.Item>
