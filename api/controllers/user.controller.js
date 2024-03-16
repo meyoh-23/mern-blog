ef8cd56e1e20ca4801109b8742212d16297da8fc
Felix Ouma <moxiemeyoh@gmail.com>
2024-03-16 16:26:36 +0300

    Add: Update Profile API - mimnor issies with the req.params

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,43 +7,43 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async ( req, res, next) => {
-    
-    if (req.user.Id !== req.params.userId) {
-        return next(errorHandler(403, "You are not allowed to update this user!"));
+    if(req.user.Id !== req.params.userId.substring(1)) {
+        return next(errorHandler(403, "You are not allowed to edit this account!"))
     };
     if (req.body.password) {
         if (req.body.password.length < 6) {
-            return next(errorHandler(403, 'Password should be at least 6 char long!'));
-        }
-        hashedPassword = bcryptjs.hashSync(req.body.password, 10);
+            return next(errorHandler(400, 'Password should be at least 6 characterss longs!'));
+        };
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
     if (req.body.username) {
-        if (req.body.username.length < 7 || req.body.username.length > 20) {
-            return next(
-                errorHandler(400, 'Username must be between 7 and 20 characters')
-            );
-            }
-        if (req.body.username.includes(' ')) {
-            return next(errorHandler(400, 'Username cannot contain spaces'));
+        if (req.body.username.length < 7 || req.body.username.length >20 ) {
+            return next(errorHandler(400, 'Username sholud be between 7 and 20 chars long!'))
         }
-        if (req.body.username !== req.body.username.toLowerCase()) {
-            return next(errorHandler(400, 'Username must be lowercase'));
+        if (req.body.username.includes(" ")) {
+            return next(errorHandler(400, 'USername cannot contain spaces!'));
+        }
+        if (req.body.username !== req.body.username.toLowerCase() ) {
+            return next(errorHandler(400, 'username munst be in lowercase!'));
         }
         if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
             return next(
                 errorHandler(400, 'Username can only contain letters and numbers')
-        );
+            );
         }
     }
-    // update the user details now
+
+    // update the user details
     try {
-        const updatedUser = await User.findOneAndUpdate(req.params.userId, {
+        const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId.substring(1),
+        {
             $set: {
-                username: req.body.username,
-                email: req.body.email,
-                profilePicture: req.body.profilePicture,
-                password: req.body.password,
-            }
+            username: req.body.username,
+            email: req.body.email,
+            profilePicture: req.body.profilePicture,
+            password: req.body.password,
+            },
         },
         { new: true }
         );
@@ -52,4 +52,4 @@ export const updateUser = async ( req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
